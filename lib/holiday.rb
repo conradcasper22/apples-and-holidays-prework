144dbaa53273d7442425d359cca86d5a808d34aa
Conrad Casper <conradcasper@icloud.com>
2019-04-17 22:18:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -128,7 +128,7 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! << ":"
-    
+    holiday.each do |holiday, supplies|
 
 end
 
